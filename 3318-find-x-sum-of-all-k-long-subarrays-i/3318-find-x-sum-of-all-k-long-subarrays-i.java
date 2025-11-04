d9b9beefb609d5d63cbad39b63ba0e238dd481b9
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2025-11-04 23:01:37 +0530

    Time: 14 ms (31.38%), Space: 47.2 MB (7.59%) - LeetHub

---
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+
+        for (int i = 0; i <= n - k; i++) {
+
+            Map<Integer, Integer> freq = new HashMap<>();
+            for (int j = i; j < i + k; j++) {
+                freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
+            }
+
+            List<int[]> list = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+                list.add(new int[] { e.getKey(), e.getValue() });
+            }
+
+            list.sort((a, b) -> {
+                if (b[1] != a[1])
+                    return b[1] - a[1];
+                return b[0] - a[0];
+            });
+
+            Set<Integer> keep = new HashSet<>();
+            for (int j = 0; j < Math.min(x, list.size()); j++) {
+                keep.add(list.get(j)[0]);
+            }
+
+        
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                if (keep.contains(nums[j]))
+                    sum += nums[j];
+            }
+
+            ans[i] = sum;
+        }
+
+        return ans;
+    }
+}
