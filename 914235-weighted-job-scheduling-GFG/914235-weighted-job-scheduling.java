c65c3c83b46b5cf82dc72ff9bf770bd91fdc294d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2025-11-07 23:51:04 +0530

    Added solution - LeetHub

---
+++ 914235-weighted-job-scheduling-GFG/914235-weighted-job-scheduling.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[][] jobs) {
+        int n = jobs.length;
+
+        
+        Arrays.sort(jobs, (a, b) -> Integer.compare(a[1], b[1]));
+
+       
+        int[] dp = new int[n];
+        dp[0] = jobs[0][2];  
+
+        for (int i = 1; i < n; i++) {
+       
+            int includeProfit = jobs[i][2];
+
+           
+            int last = findLastNonOverlapping(jobs, i);
+            if (last != -1)
+                includeProfit += dp[last];
+
+          
+            dp[i] = Math.max(dp[i - 1], includeProfit);
+        }
+
+        return dp[n - 1];
+    }
+
+   
+    private int findLastNonOverlapping(int[][] jobs, int index) {
+        int low = 0, high = index - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (jobs[mid][1] <= jobs[index][0]) {
+                ans = mid;
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
