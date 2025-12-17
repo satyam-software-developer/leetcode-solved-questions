7e509713343ecef5faf35c82a970060e54e3e832
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2025-12-17 23:42:30 +0530

    Added solution - LeetHub

---
+++ 708874-overlapping-intervals-GFG/708874-overlapping-intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        // Code here
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        ArrayList<int[]> result = new ArrayList<>();
+        int start = arr[0][0];
+        int end = arr[0][1];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] <= end) {
+                end = Math.max(end, arr[i][1]);
+            } else {
+                result.add(new int[]{start, end});
+                start = arr[i][0];
+                end = arr[i][1];
+            }
+        }
+
+        result.add(new int[]{start, end});
+        return result;
+        
+    }
+}
