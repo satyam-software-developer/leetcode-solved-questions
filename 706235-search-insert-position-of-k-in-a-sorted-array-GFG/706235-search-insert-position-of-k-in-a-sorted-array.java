e3292b098f860e5a78e478e458cb1da99f68bfff
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2025-12-20 23:04:20 +0530

    Added solution - LeetHub

---
+++ 706235-search-insert-position-of-k-in-a-sorted-array-GFG/706235-search-insert-position-of-k-in-a-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsertK(int arr[], int k) {
+        // code here
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == k) return mid;
+            if (arr[mid] < k) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+        
+    }
+};
