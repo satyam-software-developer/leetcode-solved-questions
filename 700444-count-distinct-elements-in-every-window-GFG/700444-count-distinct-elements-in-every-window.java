7bcf368f6a721ce4fbd37bf30d232e93a68f9bae
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-01-07 23:27:13 +0530

    Added solution - LeetHub

---
+++ 700444-count-distinct-elements-in-every-window-GFG/700444-count-distinct-elements-in-every-window.java
@@ -0,0 +1,27 @@
+class Solution {
+    ArrayList<Integer> countDistinct(int arr[], int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        
+        result.add(map.size());
+        
+        for (int i = k; i < arr.length; i++) {
+            int out = arr[i - k];
+            map.put(out, map.get(out) - 1);
+            if (map.get(out) == 0) {
+                map.remove(out);
+            }
+            
+            int in = arr[i];
+            map.put(in, map.getOrDefault(in, 0) + 1);
+            
+            result.add(map.size());
+        }
+        
+        return result;
+    }
+}
