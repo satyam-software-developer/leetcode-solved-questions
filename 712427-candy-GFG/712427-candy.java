46dff6c0807a62840660af5d5c2eb84dc091c989
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-01-15 23:38:15 +0530

    Added solution - LeetHub

---
+++ 712427-candy-GFG/712427-candy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minCandy(int arr[]) {
+        int n = arr.length;
+        if (n == 0) return 0;
+
+        int[] candies = new int[n];
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > arr[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int total = 0;
+        for (int c : candies) {
+            total += c;
+        }
+
+        return total;
+    }
+}
