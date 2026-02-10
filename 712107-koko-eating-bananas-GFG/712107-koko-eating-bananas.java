6131d7c345f4bf7dce4cce9850d7db84149f913a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-02-10 23:48:36 +0530

    Added solution - LeetHub

---
+++ 712107-koko-eating-bananas-GFG/712107-koko-eating-bananas.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int kokoEat(int[] arr, int k) {
+        int low = 1;
+        int high = 0;
+        for (int x : arr) {
+            high = Math.max(high, x);
+        }
+
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long hours = 0;
+
+            for (int x : arr) {
+                hours += (x + mid - 1) / mid;
+            }
+
+            if (hours <= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
