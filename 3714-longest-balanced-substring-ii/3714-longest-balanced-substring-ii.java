5891979289a8d51c4317d6348c457584814a1e90
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-02-14 23:57:46 +0530

    Time: 327 ms (41.79%), Space: 111.1 MB (87.06%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.java
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.java
@@ -1,87 +1,110 @@
+/*
+   Logic: Longest Balanced Subarray using Prefix Difference tracking.
+   If (CountA - CountB) is the same at two indices, the subarray between them is balanced.
+*/
 class Solution {
     public int longestBalanced(String s) {
-        char[] c = s.toCharArray();
-        int n = c.length;
+        int ans = 0;
 
-        int cur_a = 0, cur_b = 0, cur_c = 0;
-        int max_a = 0, max_b = 0, max_c = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (c[i] == 'a') {
-                cur_a = (i > 0 && c[i - 1] == 'a') ? cur_a + 1 : 1;
-                max_a = Math.max(max_a, cur_a);
-            } else if (c[i] == 'b') {
-                cur_b = (i > 0 && c[i - 1] == 'b') ? cur_b + 1 : 1;
-                max_b = Math.max(max_b, cur_b);
-            } else {
-                cur_c = (i > 0 && c[i - 1] == 'c') ? cur_c + 1 : 1;
-                max_c = Math.max(max_c, cur_c);
+        // CASE 1: Longest subarray with only one type of character
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            int j = i, cnt = 0;
+            while (j < s.length() && s.charAt(j) == curr) {
+                cnt++;
+                j++;
             }
+            ans = Math.max(cnt, ans);
+            i = j - 1;
         }
 
-        int res = Math.max(Math.max(max_a, max_b), max_c);
+        // CASE 2: Longest subarray with exactly two types of characters in equal counts
+        // Sub-case: 'a' and 'b' balance
+        for (int i = 0; i < s.length(); i++) {
+            int cnta = 0, cntb = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            int j = i;
+            map.put(0, j - 1); 
+            while (j < s.length()) {
+                char c = s.charAt(j);
+                if (c == 'a') cnta++;
+                else if (c == 'b') cntb++;
+                else break; 
+                
+                int diff = cnta - cntb;
+                if (map.containsKey(diff)) ans = Math.max(ans, j - map.get(diff));
+                else map.put(diff, j);
+                j++;
+            }
+            i = j;
+        }
 
-        res = Math.max(res, find2(c, 'a', 'b'));
-        res = Math.max(res, find2(c, 'a', 'c'));
-        res = Math.max(res, find2(c, 'b', 'c'));
+       // Sub-case: 'b' and 'c' balance
+        for (int i = 0; i < s.length(); i++) {
+            int cntb = 0, cntc = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            int j = i;
+            map.put(0, j - 1);
+            while (j < s.length()) {
+                char c = s.charAt(j);
+                if (c == 'c') cntc++;
+                else if (c == 'b') cntb++;
+                else break; // Stop if 'a' is encountered
 
-        res = Math.max(res, find3(c));
-
-        return res;
-    }
-
-    private int find2(char[] c, char x, char y) {
-        int n = c.length, max_len = 0;
-        int[] first = new int[2 * n + 1];
-        Arrays.fill(first, -2);
-
-        int clear_idx = -1, diff = n;
-        first[diff] = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (c[i] != x && c[i] != y) {
-                clear_idx = i;
-                diff = n;
-                first[diff] = clear_idx;
-            } else {
-                if (c[i] == x)
-                    diff++;
-                else
-                    diff--;
-
-                if (first[diff] < clear_idx) {
-                    first[diff] = i;
+                int diff = cntb - cntc;
+                if (map.containsKey(diff)) {
+                    ans = Math.max(ans, j - map.get(diff));
                 } else {
-                    max_len = Math.max(max_len, i - first[diff]);
+                    map.put(diff, j);
                 }
+                j++;
             }
+            i = j;
         }
 
-        return max_len;
-    }
+        // Sub-case: 'a' and 'c' balance
+        for (int i = 0; i < s.length(); i++) {
+            int cnta = 0, cntc = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            int j = i;
+            map.put(0, j - 1);
+            while (j < s.length()) {
+                char c = s.charAt(j);
+                if (c == 'c') cntc++;
+                else if (c == 'a') cnta++;
+                else break; // Stop if 'b' is encountered
 
-    private int find3(char[] c) {
-        long state = Long.MAX_VALUE / 2;
-        Map<Long, Integer> first = new HashMap<>();
-        first.put(state, -1);
-
-        int max_len = 0;
-
-        for (int i = 0; i < c.length; i++) {
-            if (c[i] == 'a')
-                state += 1_000_001;
-            else if (c[i] == 'b')
-                state -= 1_000_000;
-            else
-                state--;
-
-            if (first.containsKey(state)) {
-                max_len = Math.max(max_len, i - first.get(state));
-            } else {
-                first.put(state, i);
+                int diff = cnta - cntc;
+                if (map.containsKey(diff)) {
+                    ans = Math.max(ans, j - map.get(diff));
+                } else {
+                    map.put(diff, j);
+                }
+                j++;
             }
+            i = j;
         }
 
-        return max_len;
+        // CASE 3: Longest subarray with all three characters in equal counts
+        int cnta = 0, cntb = 0, cntc = 0;
+        Map<String, Integer> map3 = new HashMap<>();
+        map3.put("0,0", -1); 
+        int j = 0;
+        while (j < s.length()) {
+            char c = s.charAt(j);
+            if (c == 'c') cntc++;
+            else if (c == 'a') cnta++;
+            else cntb++;
+
+            int diff1 = cnta - cntb;
+            int diff2 = cnta - cntc;
+            String skey = diff1 + "," + diff2;
+
+            if (map3.containsKey(skey)) ans = Math.max(ans, j - map3.get(skey));
+            else map3.put(skey, j);
+            j++;
+        }
+
+        return ans;
     }
 }
