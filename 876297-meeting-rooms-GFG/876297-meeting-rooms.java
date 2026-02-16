66b4f773103abff884252aa3b6134244e19caca7
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-02-16 23:43:29 +0530

    Added solution - LeetHub

---
+++ 876297-meeting-rooms-GFG/876297-meeting-rooms.java
@@ -0,0 +1,9 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        java.util.Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] < arr[i - 1][1]) return false;
+        }
+        return true;
+    }
+}
