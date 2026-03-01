4da1bd5a4f1537e24973efcdd280e2526fd657d5
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-03-01 23:46:43 +0530

    Added solution - LeetHub

---
+++ 702806-move-all-zeroes-to-end-GFG/702806-move-all-zeroes-to-end.java
@@ -0,0 +1,16 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index++] = arr[i];
+            }
+        }
+        
+        while (index < n) {
+            arr[index++] = 0;
+        }
+    }
+}
