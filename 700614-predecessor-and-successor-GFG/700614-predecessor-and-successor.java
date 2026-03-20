ba39782ec41a05a8b9b6443b534c0ca0191265e0
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-03-20 23:33:31 +0530

    Added solution - LeetHub

---
+++ 700614-predecessor-and-successor-GFG/700614-predecessor-and-successor.java
@@ -0,0 +1,42 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int x) {
+        data = x;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        Node pre = null, suc = null;
+        Node curr = root;
+
+        while (curr != null) {
+            if (curr.data < key) {
+                pre = curr;
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+
+        curr = root;
+
+        while (curr != null) {
+            if (curr.data > key) {
+                suc = curr;
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+
+        ArrayList<Node> res = new ArrayList<>();
+        res.add(pre);
+        res.add(suc);
+        return res;
+    }
+}
