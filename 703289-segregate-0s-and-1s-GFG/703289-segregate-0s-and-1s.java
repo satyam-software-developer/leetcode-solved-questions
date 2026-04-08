625fcce9941170c304d6eef35d65dc4b72af9f91
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-04-08 23:38:22 +0530

    Added solution - LeetHub

---
+++ 703289-segregate-0s-and-1s-GFG/703289-segregate-0s-and-1s.java
@@ -0,0 +1,16 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            while (left < right && arr[left] == 0) left++;
+            while (left < right && arr[right] == 1) right--;
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
