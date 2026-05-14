429d6a4303979a9dd180c0bf4e30a88ed1fc00ca
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-14 23:44:27 +0530

    Added solution - LeetHub

---
+++ 880847-search-for-subarray-GFG/880847-search-for-subarray.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> search(int[] a, int[] b) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int n = a.length;
+        int m = b.length;
+        
+        int[] lps = new int[m];
+        
+        int len = 0;
+        int i = 1;
+        
+        while (i < m) {
+            if (b[i] == b[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        int x = 0;
+        int y = 0;
+        
+        while (x < n) {
+            if (a[x] == b[y]) {
+                x++;
+                y++;
+            }
+            
+            if (y == m) {
+                res.add(x - y);
+                y = lps[y - 1];
+            } else if (x < n && a[x] != b[y]) {
+                if (y != 0) {
+                    y = lps[y - 1];
+                } else {
+                    x++;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
