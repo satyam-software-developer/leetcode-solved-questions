830a0ead42e44bd577c8fe892e47d871f287215d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-16 23:48:13 +0530

    Added solution - LeetHub

---
+++ 703420-not-a-subset-sum-GFG/703420-not-a-subset-sum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findSmallest(int[] arr) {
+        Arrays.sort(arr);
+        
+        long res = 1;
+        
+        for (int num : arr) {
+            if (num > res) {
+                break;
+            }
+            res += num;
+        }
+        
+        return (int) res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
