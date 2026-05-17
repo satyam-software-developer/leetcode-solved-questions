86bf385cc2874e28618cd14e817a908d36222c76
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-17 23:39:26 +0530

    Added solution - LeetHub

---
+++ 714010-make-the-array-beautiful-GFG/714010-make-the-array-beautiful.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    List<Integer> makeBeautiful(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : arr) {
+            if (!stack.isEmpty() &&
+                ((stack.peek() >= 0 && num < 0) || (stack.peek() < 0 && num >= 0))) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        return new ArrayList<>(stack);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
