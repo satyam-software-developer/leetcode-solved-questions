6e2083c154cdff09610c643b655d40e11cc8784a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-21 23:29:41 +0530

    Added solution - LeetHub

---
+++ 703726-check-if-all-bits-set-GFG/703726-check-if-all-bits-set.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isBitSet(int n) {
+        return n > 0 && (n & (n + 1)) == 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
