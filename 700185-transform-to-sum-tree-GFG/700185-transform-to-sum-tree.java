9239d03bfecc0d58a0a806d4d99984a7f66f753a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-23 23:30:03 +0530

    Added solution - LeetHub

---
+++ 700185-transform-to-sum-tree-GFG/700185-transform-to-sum-tree.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void toSumTree(Node root) {
+        solve(root);
+    }
+
+    private int solve(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int oldValue = root.data;
+
+        int leftSum = solve(root.left);
+        int rightSum = solve(root.right);
+
+        root.data = leftSum + rightSum;
+
+        return oldValue + root.data;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
