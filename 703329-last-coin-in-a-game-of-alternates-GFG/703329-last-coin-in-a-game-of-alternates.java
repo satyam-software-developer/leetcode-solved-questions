4fa174967a2dac440706eae712b70ac1a9e4d955
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-24 23:39:44 +0530

    Added solution - LeetHub

---
+++ 703329-last-coin-in-a-game-of-alternates-GFG/703329-last-coin-in-a-game-of-alternates.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coin(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] > arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return arr[left];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
