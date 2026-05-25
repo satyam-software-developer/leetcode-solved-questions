82ccc520aa50fa8a36e4745a4cc99f2d99a41c23
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-25 23:41:03 +0530

    Added solution - LeetHub

---
+++ 703193-elements-in-the-range-GFG/703193-elements-in-the-range.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkElements(int start, int end, int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        for (int i = start; i <= end; i++) {
+            if (!set.contains(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
