11bcd0935807d9196e7bde0727a1cb7ed7354549
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-27 23:46:25 +0530

    Added solution - LeetHub

---
+++ 714215-wifi-range-GFG/714215-wifi-range.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean wifiRange(String s, int x) {
+        int n = s.length();
+        boolean[] covered = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                int left = Math.max(0, i - x);
+                int right = Math.min(n - 1, i + x);
+
+                for (int j = left; j <= right; j++) {
+                    covered[j] = true;
+                }
+            }
+        }
+
+        for (boolean room : covered) {
+            if (!room) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
