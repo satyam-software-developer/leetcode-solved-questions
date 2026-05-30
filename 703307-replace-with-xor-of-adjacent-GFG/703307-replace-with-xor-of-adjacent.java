587c923453d6b38f8496cbdd505a0cc71e9fbc83
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-05-30 23:24:02 +0530

    Added solution - LeetHub

---
+++ 703307-replace-with-xor-of-adjacent-GFG/703307-replace-with-xor-of-adjacent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void replaceElements(int[] arr) {
+        int n = arr.length;
+        int[] temp = arr.clone();
+
+        arr[0] = temp[0] ^ temp[1];
+
+        for (int i = 1; i < n - 1; i++) {
+            arr[i] = temp[i - 1] ^ temp[i + 1];
+        }
+
+        arr[n - 1] = temp[n - 2] ^ temp[n - 1];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
