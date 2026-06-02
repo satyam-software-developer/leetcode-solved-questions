d5a963db73fdc3c8fdfa4c7df0ba7dd298705bef
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-02 23:37:43 +0530

    Added solution - LeetHub

---
+++ 702754-pairs-with-certain-difference-GFG/702754-pairs-with-certain-difference.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class Solution {
+    public int sumDiffPairs(int[] arr, int k) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int[] dp = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1];
+
+            if (arr[i] - arr[i - 1] < k) {
+                int sum = arr[i] + arr[i - 1];
+                if (i >= 2) {
+                    sum += dp[i - 2];
+                }
+                dp[i] = Math.max(dp[i], sum);
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
