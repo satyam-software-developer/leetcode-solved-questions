67e3a3f3a62bdd53dca0193dc815c03a56f066fd
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-04 23:27:58 +0530

    Added solution - LeetHub

---
+++ 703404-substring-with-max-zero-one-diff-GFG/703404-substring-with-max-zero-one-diff.java
@@ -0,0 +1,18 @@
+class Solution {
+    int maxSubstring(String s) {
+        int maxSoFar = Integer.MIN_VALUE;
+        int curr = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) == '0' ? 1 : -1;
+            curr = Math.max(val, curr + val);
+            maxSoFar = Math.max(maxSoFar, curr);
+        }
+
+        return maxSoFar <= 0 ? -1 : maxSoFar;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
