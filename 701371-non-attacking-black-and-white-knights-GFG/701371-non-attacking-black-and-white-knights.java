e5aed9e7f2a0477338dacfb058608e321d4bfd4e
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-06 23:46:00 +0530

    Added solution - LeetHub

---
+++ 701371-non-attacking-black-and-white-knights-GFG/701371-non-attacking-black-and-white-knights.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numOfWays(int n, int m) {
+        long total = (long) n * m;
+        long placements = total * (total - 1);
+
+        long attacks = 0;
+        attacks += 4L * (n - 1) * (m - 2 >= 0 ? m - 2 : 0);
+        attacks += 4L * (n - 2 >= 0 ? n - 2 : 0) * (m - 1);
+
+        return (int) (placements - attacks);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
