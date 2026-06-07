54f2960eb33218902b18c0e71fa8dd36b0be8346
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-07 23:25:55 +0530

    Added solution - LeetHub

---
+++ 705281-finding-profession-GFG/705281-finding-profession.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String profession(int level, int pos) {
+        int flips = Integer.bitCount(pos - 1);
+        return (flips & 1) == 0 ? "Engineer" : "Doctor";
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
