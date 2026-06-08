994060c2b18e2dbb5d6d872eb05f0eb706f5c002
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-08 23:52:04 +0530

    Time: 16 ms (11.66%), Space: 148.9 MB (98.01%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> less = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num == pivot) {
+                equal.add(num);
+            } else {
+                greater.add(num);
+            }
+        }
+
+        int index = 0;
+        for (int num : less) {
+            nums[index++] = num;
+        }
+        for (int num : equal) {
+            nums[index++] = num;
+        }
+        for (int num : greater) {
+            nums[index++] = num;
+        }
+
+        return nums;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
