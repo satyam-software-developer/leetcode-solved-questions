7c9694c096a0dac498717d592eef91aef5f38b88
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 00:58:04 +0530

    Added solution - LeetHub

---
+++ 700765-start-coding-GFG/700765-start-coding.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    cout<<"Hello World"<<endl;
+    
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
