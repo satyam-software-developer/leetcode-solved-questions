1f4957c8a7c44d787aa2bb82225c75df8def6bcd
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 12:08:41 +0530

    Added solution - LeetHub

---
+++ 704102-swap-the-numbers-GFG/704102-swap-the-numbers.cpp
@@ -0,0 +1,12 @@
+void swap(int &a, int &b) {
+	// code here
+	int temp = a;
+	a = b;
+	b = temp;
+	
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
