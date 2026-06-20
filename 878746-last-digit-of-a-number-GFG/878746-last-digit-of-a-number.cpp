8c77927e1e844be309de422717c33eae90951698
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 13:04:01 +0530

    Added solution - LeetHub

---
+++ 878746-last-digit-of-a-number-GFG/878746-last-digit-of-a-number.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>> n;
+	
+	// code here
+	cout << abs(n % 10) << endl;
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
