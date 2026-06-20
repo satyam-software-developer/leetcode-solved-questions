c2457005ba65ecf1724086fc66f10a3c88e6495f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 23:37:39 +0530

    Added solution - LeetHub

---
+++ 704709-last-digit-of-ab-GFG/704709-last-digit-of-ab.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getLastDigit(String a, String b) {
+        if (b.equals("0")) return 1;
+
+        int base = a.charAt(a.length() - 1) - '0';
+        int exp = 0;
+
+        for (int i = 0; i < b.length(); i++) {
+            exp = (exp * 10 + (b.charAt(i) - '0')) % 4;
+        }
+
+        if (exp == 0) exp = 4;
+
+        int result = 1;
+        for (int i = 0; i < exp; i++) {
+            result = (result * base) % 10;
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
