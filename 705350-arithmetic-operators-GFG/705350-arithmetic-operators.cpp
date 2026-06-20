62b9fa35c4befd22755a3f2c607a3fdf4e78bb2f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 11:46:32 +0530

    Added solution - LeetHub

---
+++ 705350-arithmetic-operators-GFG/705350-arithmetic-operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin>> x>> y;
+	
+	// code here
+	int p = x + y;
+	int q = x - y;
+	int r = x * y;
+	int s = x / y;
+	int t = x % y;
+	
+	// The below code prints the output
+	cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
