7c78d451db6e9a9e1a890e5a9247da17786ea1ca
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 12:41:46 +0530

    Added solution - LeetHub

---
+++ 704473-even-positioned-characters-GFG/704473-even-positioned-characters.cpp
@@ -0,0 +1,15 @@
+void utility(string s) {
+	// length of string is given by s.length()
+	// character at any index i is given by s[i]
+	
+	// Write your code here
+	for (int i = 0; i < s.length(); i += 2) {
+		cout << (s[i]);
+	}
+	
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
