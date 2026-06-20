6a021eafc392c59ca519ffc09eea4d5de149304b
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-20 12:01:28 +0530

    Added solution - LeetHub

---
+++ 705361-right-angle-triangle-pattern-GFG/705361-right-angle-triangle-pattern.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    // code here
+     for(int i = 1; i <= n; i++){
+            for(int j = 1; j <=i; j++){
+                cout << ("* ");
+            }
+            
+            cout<<endl;
+        }
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
