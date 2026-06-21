c18f85bfd6d7fffadd21d1e32942475e532aedb5
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 13:14:15 +0530

    Added solution - LeetHub

---
+++ 878684-type-conversion-GFG/878684-type-conversion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double d;
+	cin>> d;
+	
+	// code here
+	int result = (int) d;
+	
+	cout << result << endl;
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
