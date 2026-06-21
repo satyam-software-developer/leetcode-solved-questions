053d96555d3a744d7afcce14fa9d69a11067602f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 14:06:06 +0530

    Added solution - LeetHub

---
+++ 878871-even-odd-game-GFG/878871-even-odd-game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>> n;
+	
+	// code here
+	if (n % 2 == 1) {
+		cout << "You" << endl;
+	} else {
+		cout << "Friend" << endl;
+	}
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
