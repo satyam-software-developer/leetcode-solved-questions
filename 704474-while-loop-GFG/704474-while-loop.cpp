767b7ed1383b1182c6220db7404731806f14efeb
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 13:42:48 +0530

    Added solution - LeetHub

---
+++ 704474-while-loop-GFG/704474-while-loop.cpp
@@ -0,0 +1,15 @@
+class Solution {
+	public:
+	void utility(int x) {
+		// code here
+		for (int i = x; i >= 0; i--) {
+			cout <<  i << " ";
+		}
+		
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
