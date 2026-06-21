003c9d38f5e0e9672ff2be55bd55467c5e2e1e0d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 13:23:21 +0530

    Added solution - LeetHub

---
+++ 704101-print-with-separator-GFG/704101-print-with-separator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	string a, b, separator;
+	cin>> a>> b>> separator;
+	
+	// code here
+	string result = a + separator + b;
+	
+	cout << result << endl;
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
