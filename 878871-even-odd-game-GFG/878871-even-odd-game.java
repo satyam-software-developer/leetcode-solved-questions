54b7f743d7f6dbdddb288a2f42fe92fa3558b74e
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 14:04:31 +0530

    Added solution - LeetHub

---
+++ 878871-even-odd-game-GFG/878871-even-odd-game.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// code here
+		if (n % 2 == 1) {
+			System.out.println("You");
+		} else {
+			System.out.println("Friend");
+		}
+		
+		sc.close();
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
