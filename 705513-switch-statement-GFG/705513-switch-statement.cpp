20995ee8ace0712bcd912c5bc0918ad3d5ecaef9
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 11:46:37 +0530

    Added solution - LeetHub

---
+++ 705513-switch-statement-GFG/705513-switch-statement.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>> n;
+	
+	// code here
+	switch (n) {
+		case 1:
+		cout << "One" << endl;
+		break;
+		case 2:
+		cout << "Two" << endl;
+		break;
+		case 3:
+		cout << "Three" << endl;
+		break;
+		case 4:
+		cout << "Four" << endl;
+		break;
+		case 5:
+		cout << "Five" << endl;
+		break;
+		case 6:
+		cout << "Six" << endl;
+		break;
+		case 7:
+		cout << "Seven" << endl;
+		break;
+		case 8:
+		cout << "Eight" << endl;
+		break;
+		case 9:
+		cout << "Nine" << endl;
+		break;
+		default:
+		cout << "Unknown" << endl;
+		
+		return 0;
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
