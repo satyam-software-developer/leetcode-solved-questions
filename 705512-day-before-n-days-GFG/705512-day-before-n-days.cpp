45637570425c6a1e86c0ec1d6d534d05a54ef654
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 12:55:00 +0530

    Added solution - LeetHub

---
+++ 705512-day-before-n-days-GFG/705512-day-before-n-days.cpp
@@ -0,0 +1,11 @@
+int nthDay(int d, int n) {
+	
+	// write your code here
+	n %= 7;
+	return (d - n + 7) % 7;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
