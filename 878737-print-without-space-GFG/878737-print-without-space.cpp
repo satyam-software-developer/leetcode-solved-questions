a4483970be6754979330c1343a5a1737073b531d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 12:40:45 +0530

    Added solution - LeetHub

---
+++ 878737-print-without-space-GFG/878737-print-without-space.cpp
@@ -0,0 +1,14 @@
+class Solution {
+	public:
+	void concatAndPrint(string& a, string& b) {
+		// code here
+		
+		cout << a << b << endl;
+		
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
