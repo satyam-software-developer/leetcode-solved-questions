c3c1b99391f078c3625c59211a126297243a255d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 12:40:02 +0530

    Added solution - LeetHub

---
+++ 878737-print-without-space-GFG/878737-print-without-space.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void concatAndPrint(String a, String b) {
+        // code here
+        System.out.println(a + b);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
