4ed494c47ee73fce3205301fdcde7c74a7508d56
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 23:50:27 +0530

    Added solution - LeetHub

---
+++ 706064-maximum-area-between-bars-GFG/706064-maximum-area-between-bars.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(List<Integer> height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int ans = 0;
+
+        while (left < right) {
+            int h = Math.min(height.get(left), height.get(right));
+            ans = Math.max(ans, h * (right - left - 1));
+
+            if (height.get(left) < height.get(right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
