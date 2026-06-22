a050361b01682d272e81b2ad84ec8fd348763eda
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 11:40:59 +0530

    Added solution - LeetHub

---
+++ 705513-switch-statement-GFG/705513-switch-statement.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// code here
+		switch (n) {
+			case 1:
+			System.out.println("One");
+			break;
+			case 2:
+			System.out.println("Two");
+			break;
+			case 3:
+			System.out.println("Three");
+			break;
+			case 4:
+			System.out.println("Four");
+			break;
+			case 5:
+			System.out.println("Five");
+			break;
+			case 6:
+			System.out.println("Six");
+			break;
+			case 7:
+			System.out.println("Seven");
+			break;
+			case 8:
+			System.out.println("Eight");
+			break;
+			case 9:
+			System.out.println("Nine");
+			break;
+			default:
+			System.out.println("Unknown");
+			
+		}
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
