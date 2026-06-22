2f0ba42d800f734666b9a484cb53840c9a21d3fc
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 11:53:54 +0530

    Added solution - LeetHub

---
+++ 704034-print-with-space-GFG/704034-print-with-space.cpp
@@ -0,0 +1,11 @@
+void utility() {
+	string a, b;
+	cin>> a>> b;
+	// Write your code below that prints a <space> b
+	cout << a << " " << b << endl;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
