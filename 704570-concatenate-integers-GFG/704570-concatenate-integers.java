c0d490ae5542b593a1be2b297c06bc10c9bcacaf
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-22 12:12:41 +0530

    Added solution - LeetHub

---
+++ 704570-concatenate-integers-GFG/704570-concatenate-integers.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // code here
+        System.out.println("" + a + b);
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
