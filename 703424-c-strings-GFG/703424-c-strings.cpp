5ae4a9ea2c5a5901fddafc9a380dcbe58db827da
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 15:34:19 +0530

    Added solution - LeetHub

---
+++ 703424-c-strings-GFG/703424-c-strings.cpp
@@ -0,0 +1,11 @@
+string conCat(string s1, string s2) {
+	// code here.
+	string result = s1 + s2;
+	
+	return result;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
