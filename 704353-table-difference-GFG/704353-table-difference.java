86a6d5a33da6b1176afb7f1fb62b80a0afcf4cb6
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 15:00:43 +0530

    Added solution - LeetHub

---
+++ 704353-table-difference-GFG/704353-table-difference.java
@@ -0,0 +1,16 @@
+class Solution {
+	public static void difference(int n1, int n2) {
+		
+		// Write your code here
+		int diff = n1 - n2;
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(diff * i + " ");
+		}
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
