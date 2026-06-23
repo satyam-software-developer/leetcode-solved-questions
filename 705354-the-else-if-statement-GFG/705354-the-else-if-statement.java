bdec5c1279e18bde4d2dcea4e18585ae0c689ea0
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 15:25:40 +0530

    Added solution - LeetHub

---
+++ 705354-the-else-if-statement-GFG/705354-the-else-if-statement.java
@@ -0,0 +1,18 @@
+class Solution {
+	public static void utility(int number) {
+		// Write the if, else if, else statements below
+		
+		if (number > 100) {
+			System.out.println("Big");
+		} else if (number < 10) {
+			System.out.println("Small");
+		} else {
+			System.out.println("Number");
+		}
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
