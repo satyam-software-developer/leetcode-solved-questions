9302994d80a611095273b5a03a6ecf9ed233cdef
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 13:32:36 +0530

    Added solution - LeetHub

---
+++ 878742-evaluate-formulae-GFG/878742-evaluate-formulae.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin>> a>> b>> c>> d;
+	
+	// code here
+	int result = (a + b)/c + d;
+	
+	cout << result << endl;
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
