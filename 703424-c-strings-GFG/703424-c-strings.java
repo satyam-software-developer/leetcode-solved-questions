cc0a988ab61cf18cd4d6e059e3cf8925dc918768
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 15:33:32 +0530

    Added solution - LeetHub

---
+++ 703424-c-strings-GFG/703424-c-strings.java
@@ -0,0 +1,12 @@
+class Sol {
+    String conCat(String s1, String s2) {
+        // code here.
+        String result = s1 + s2;
+        
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
