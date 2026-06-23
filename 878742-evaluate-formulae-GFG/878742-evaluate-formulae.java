d3819bdc70a74637e6bbbba142d5bc95bd1159ac
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-23 13:30:47 +0530

    Added solution - LeetHub

---
+++ 878742-evaluate-formulae-GFG/878742-evaluate-formulae.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        // code here
+        int result = (a + b)/c + d;
+        
+        System.out.println(result);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
