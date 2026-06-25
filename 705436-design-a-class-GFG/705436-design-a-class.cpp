41dc2ae0047616e0f3ea79a06b1d448b1505885a
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 14:14:15 +0530

    Added solution - LeetHub

---
+++ 705436-design-a-class-GFG/705436-design-a-class.cpp
@@ -0,0 +1,15 @@
+// Design a class as described in the question
+// code here
+
+class MyClass {
+	
+	public:
+	void display() {
+		cout << "Hello World" << endl;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
