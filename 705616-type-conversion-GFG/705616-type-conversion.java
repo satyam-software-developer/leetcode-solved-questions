3d63082ed4a6af16a810b75637e9ebc425a8c17c
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 13:28:28 +0530

    Added solution - LeetHub

---
+++ 705616-type-conversion-GFG/705616-type-conversion.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		double d = sc.nextDouble();
+		
+		// code here
+		int result = (int) d;
+		
+		System.out.println(result);
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
