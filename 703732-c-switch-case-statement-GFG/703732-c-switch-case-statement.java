8572b244f7a023bacaf78891613b765dead6bfbd
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 12:39:39 +0530

    Added solution - LeetHub

---
+++ 703732-c-switch-case-statement-GFG/703732-c-switch-case-statement.java
@@ -0,0 +1,45 @@
+class Solution {
+	static String isInRange(int N) {
+		// code here
+		switch (N) {
+			case 1:
+			return "one";
+			
+			case 2:
+			return "two";
+			
+			case 3:
+			return "three";
+			
+			case 4:
+			return "four";
+			
+			case 5:
+			return "five";
+			
+			case 6:
+			return "six";
+			
+			case 7:
+			return "seven";
+			
+			case 8:
+			return "eight";
+			
+			case 9:
+			return "nine";
+			
+			case 10:
+			return "ten";
+			
+			default :
+			return "not in range ";
+			
+		}
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
