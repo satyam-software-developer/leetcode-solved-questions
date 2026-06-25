18d86b152e8faac48ed1dcf137ce1ae6fae45a12
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 14:09:16 +0530

    Added solution - LeetHub

---
+++ 705436-design-a-class-GFG/705436-design-a-class.java
@@ -0,0 +1,13 @@
+// Design a class as described in the question
+// code here
+
+class MyClass {
+	public void display() {
+		System.out.println("Hello World");
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
