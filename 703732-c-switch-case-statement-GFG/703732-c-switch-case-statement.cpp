4e777680e304adadd38c174773f9a447448279e2
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 12:51:50 +0530

    Added solution - LeetHub

---
+++ 703732-c-switch-case-statement-GFG/703732-c-switch-case-statement.cpp
@@ -0,0 +1,49 @@
+class Solution {
+	public:
+	void isInRange(int n) {
+		// you don't need to take input
+		// here just write code which print correct answer according to n
+		switch (n) {
+			case 1:
+			cout << "one";
+			break;
+			case 2:
+			cout << "two";
+			break;
+			case 3:
+			cout << "three";
+			break;
+			case 4:
+			cout << "four";
+			break;
+			case 5:
+			cout << "five";
+			break;
+			case 6:
+			cout << "six";
+			break;
+			case 7:
+			cout << "seven";
+			break;
+			case 8:
+			cout << "eight";
+			break;
+			case 9:
+			cout << "nine";
+			break;
+			case 10:
+			cout << "ten";
+			break;
+			
+			default :
+			cout << "not in range ";
+			
+		}
+		cout << endl;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
