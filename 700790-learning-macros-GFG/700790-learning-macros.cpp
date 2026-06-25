ebead5e19c4783f397b91d63e668a76ae9ad1cfc
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 13:53:44 +0530

    Added solution - LeetHub

---
+++ 700790-learning-macros-GFG/700790-learning-macros.cpp
@@ -0,0 +1,13 @@
+void macros(int a, int b) {
+	// See the macro defined at the top of the code. Use that macro to find a*b
+	// Use only macro to complete this task
+	
+	cout << MULTI(a, b);
+	
+	cout << endl;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
