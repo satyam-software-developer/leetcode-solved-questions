63c19920c55c0d7674e27e62b53f8423054db35b
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 13:17:44 +0530

    Added solution - LeetHub

---
+++ 704414-next-prime-number-GFG/704414-next-prime-number.cpp
@@ -0,0 +1,29 @@
+int nextPrime(int n) {
+	
+	// code here to find next prime number
+	// return next prime number
+	int num = n + 1;
+	
+	while (true) {
+		bool prime = true;
+		
+		for (int i = 2; i * i <= num; i++) {
+			if (num % i == 0) {
+				prime = false;
+				break;
+			}
+			
+		}
+		if (prime) {
+			return num;
+		}
+		
+		num++;
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
