3c00d0f7212d2c37f697a6fc02dc885c47509fc2
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 14:06:32 +0530

    Added solution - LeetHub

---
+++ 705385-decrement-array-values-GFG/705385-decrement-array-values.java
@@ -0,0 +1,17 @@
+class Solution {
+	public static int[] decrementArrayElements(int[] arr) {
+		// Code here
+		for (int i = 0; i < arr.length; i++) {
+			
+			arr[i] = arr[i] - 1;
+			
+		}
+		
+		return arr;
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
