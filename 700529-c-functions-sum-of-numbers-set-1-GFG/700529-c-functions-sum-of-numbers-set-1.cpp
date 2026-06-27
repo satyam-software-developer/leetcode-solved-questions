111fc899d12dc82c5598cdac8f16db10425d4b45
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 14:24:01 +0530

    Added solution - LeetHub

---
+++ 700529-c-functions-sum-of-numbers-set-1-GFG/700529-c-functions-sum-of-numbers-set-1.cpp
@@ -0,0 +1,17 @@
+/* Write your function here */
+
+/* The function should be named calcSum
+and accepts three parameters of integer type
+and return s the sum of the three integers */
+
+int calcSum(int a, int b, int c) {
+	// Your code here
+	int s = a + b + c;
+	
+	return s;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
