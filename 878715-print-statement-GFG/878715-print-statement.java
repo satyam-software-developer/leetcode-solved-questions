c80d02b3875766a52aae8248101dec8665c915c3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 13:51:59 +0530

    Added solution - LeetHub

---
+++ 878715-print-statement-GFG/878715-print-statement.java
@@ -0,0 +1,14 @@
+class Solution {
+	void utility(Scanner scn) {
+		// code here
+		String s = scn.nextLine();
+		
+		System.out.println(s);
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
