8a6886bb53e83e2e5b7bdc133f75546f95e420bc
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 15:03:36 +0530

    Added solution - LeetHub

---
+++ 878745-sum-of-natural-numbers-GFG/878745-sum-of-natural-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// code here
+		
+		int sum = n * (n + 1) / 2;
+		System.out.println(sum);
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
