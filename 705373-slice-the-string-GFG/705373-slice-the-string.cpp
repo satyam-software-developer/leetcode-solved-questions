42dd333ae0a55a9b0fcd0f8b098a3d080d500033
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-29 17:04:11 +0530

    Added solution - LeetHub

---
+++ 705373-slice-the-string-GFG/705373-slice-the-string.cpp
@@ -0,0 +1,8 @@
+string sliceString(string &s) {
+    // code here
+    return s.substr(1, s.size() -2);
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
