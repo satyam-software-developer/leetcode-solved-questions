fd931bb31973697340af51a7514553356ff545ab
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-29 17:00:07 +0530

    Added solution - LeetHub

---
+++ 705373-slice-the-string-GFG/705373-slice-the-string.java
@@ -0,0 +1,11 @@
+class Solution {
+	public static String sliceString(String s) {
+		// code here
+		return s.substring(1, s.length() - 1);
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
