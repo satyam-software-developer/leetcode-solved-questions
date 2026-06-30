64283dbf08b768a6d76b99cb3623b5fc95f70e44
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-30 13:37:41 +0530

    Added solution - LeetHub

---
+++ 706213-find-if-ve-ve-or-0-GFG/706213-find-if-ve-ve-or-0.cpp
@@ -0,0 +1,15 @@
+void fun(int a) {
+	// code here
+	if (a > 0) {
+		cout << "Positive";
+	} else if (a < 0) {
+		cout << "Negative";
+	} else {
+		cout << "Zero";
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
