33e08b1c7d4a26e0f29e3f9f07eb20bf3cf37191
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-30 13:46:47 +0530

    Added solution - LeetHub

---
+++ 706173-print-geeks-for-geeks-with-quotes-GFG/706173-print-geeks-for-geeks-with-quotes.java
@@ -0,0 +1,12 @@
+class GFG {
+	
+	public static void main(String[] args) {
+		// code here
+		System.out.print("\"GeeksForGeeks\"");
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
