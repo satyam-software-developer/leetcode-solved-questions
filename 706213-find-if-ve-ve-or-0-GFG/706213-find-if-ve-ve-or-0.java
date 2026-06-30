67304ed82a5392588ea7ab52a306ea5aa4bcb4bf
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-30 13:30:49 +0530

    Added solution - LeetHub

---
+++ 706213-find-if-ve-ve-or-0-GFG/706213-find-if-ve-ve-or-0.java
@@ -0,0 +1,20 @@
+class Solution {
+	void fun(int a) {
+		// code here
+		
+		if (a > 0) {
+			System.out.print("Positive");
+		} else if (a < 0) {
+			System.out.print("Negative");
+		} else {
+			
+			System.out.print("Zero");
+		}
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
