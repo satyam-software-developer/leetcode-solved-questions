42cbd7c16f47637900f8acbd91cb04467afa69f2
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-30 23:44:57 +0530

    Time: 15 ms (52.53%), Space: 45.9 MB (95.37%) - LeetHub - Time: O(?), Space: O(?)

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,23 +1,22 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] count = new int[3]; // To track occurrences of 'a', 'b', 'c'
-        int left = 0, result = 0;
-        
+        int[] count = new int[3];
+        int left = 0;
+        int ans = 0;
+
         for (int right = 0; right < s.length(); right++) {
-            count[s.charAt(right) - 'a']++; // Increment count for current character
-            
-            // While all 'a', 'b', and 'c' are present at least once
+            count[s.charAt(right) - 'a']++;
+
             while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                result += s.length() - right; // All substrings from left to end are valid
-                count[s.charAt(left) - 'a']--; // Move left pointer to shrink window
+                ans += s.length() - right;
+                count[s.charAt(left) - 'a']--;
                 left++;
             }
         }
-        
-        return result;
+
+        return ans;
     }
 }
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
