a2c6c4909b8a26fbc45fb4b255520f0aca815c19
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-01 15:45:38 +0530

    Added solution - LeetHub

---
+++ 705439-constructor-overloading-GFG/705439-constructor-overloading.cpp
@@ -0,0 +1,24 @@
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+
+class User {
+	public:
+	
+	string name;
+	
+	User() {
+		name = "Default";
+	}
+	
+	User(string name) {
+		this->name = name;
+	}
+	
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
