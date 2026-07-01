081e38460c952e97953f26629fdc49cee942d092
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-01 13:05:44 +0530

    Added solution - LeetHub

---
+++ 705440-addition-GFG/705440-addition.cpp
@@ -0,0 +1,19 @@
+// create a class addition
+// with a function add
+// this should return the sum of
+// two values(a,b) passed in the parameter
+
+// code here
+
+class Addition {
+	
+	public:
+	static int add(int a, int b) {
+		return a + b;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
