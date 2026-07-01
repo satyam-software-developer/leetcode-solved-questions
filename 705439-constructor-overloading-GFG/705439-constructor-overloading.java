ce994ef85cee6bf7e0871f4aea4f035710a4e976
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-01 15:41:53 +0530

    Added solution - LeetHub

---
+++ 705439-constructor-overloading-GFG/705439-constructor-overloading.java
@@ -0,0 +1,23 @@
+// design a class with two constructor
+// one default, the other parameterized
+
+// code here
+
+class User {
+	
+	String name;
+	
+	User() {
+		name = "Default";
+	}
+	
+	User(String name) {
+		this.name = name;
+	}
+	
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
