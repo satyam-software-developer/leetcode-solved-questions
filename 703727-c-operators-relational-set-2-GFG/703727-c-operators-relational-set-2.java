ef43e1ed3b96df66715ab1b25e5b49fbec075484
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-01 13:56:32 +0530

    Added solution - LeetHub

---
+++ 703727-c-operators-relational-set-2-GFG/703727-c-operators-relational-set-2.java
@@ -0,0 +1,21 @@
+class Solution {
+	static String compareNum(int A, int B) {
+		// code here
+		
+		if (A > B) {
+			return A + " is greater than " + B;
+		}
+		else if (A < B) {
+			return A + " is less than " + B;
+		}
+		else {
+			return A + " is equals to " + B;
+		}
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
