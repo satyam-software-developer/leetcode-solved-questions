0510ae78c9a34c613e391a1f6f54eb57dd6d0135
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 14:28:43 +0530

    Added solution - LeetHub

---
+++ 703874-c-data-types-GFG/703874-c-data-types.cpp
@@ -0,0 +1,30 @@
+class Solution {
+	public:
+	int cppIntType() {
+		// code here
+		int x;
+		cin>> x;
+		return x;
+		
+	}
+	
+	char cppCharType() {
+		
+		// code here
+		char ch;
+		cin>> ch;
+		return ch;
+	}
+	
+	float cppFloatType() {
+		// code here
+		float f;
+		cin>> f;
+		return f;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
