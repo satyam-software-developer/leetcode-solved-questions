8dda61214c0186f87b92061d4ca5ba207360d5a4
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 14:45:52 +0530

    Added solution - LeetHub

---
+++ 706212-scientific-format-GFG/706212-scientific-format.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	// code here
+	double a;
+	cin>> a;
+	
+	cout << scientific << setprecision(4) << a;
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
