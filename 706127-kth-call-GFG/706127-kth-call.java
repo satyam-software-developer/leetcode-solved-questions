76e68bc5ea94420123f8d92e94731b1c97a7a924
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 13:27:03 +0530

    Added solution - LeetHub

---
+++ 706127-kth-call-GFG/706127-kth-call.java
@@ -0,0 +1,19 @@
+class Solution {
+	
+	// declare the variable count here
+	int count = 0;
+	
+	void utility() {
+		
+		// increases the value of count by 1
+		count++;
+		
+		// print count
+		System.out.print(count);
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
