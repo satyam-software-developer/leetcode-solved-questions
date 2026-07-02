e4164613f36be4b231b2174cd17a579f62b72ac1
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 14:42:11 +0530

    Added solution - LeetHub

---
+++ 706212-scientific-format-GFG/706212-scientific-format.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		// code here
+		Scanner sc = new Scanner(System.in);
+		
+		Double a = sc.nextDouble();
+		
+		System.out.printf("%.4e", a);
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
