c81140a9fba6ca053d4456c60f54c77b3423ff25
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 13:37:56 +0530

    Added solution - LeetHub

---
+++ 700726-c-generic-sort-GFG/700726-c-generic-sort.cpp
@@ -0,0 +1,11 @@
+template <class T>
+void sortArray(vector<T>& arr) {
+	// code here
+	sort(arr.begin(), arr.end());
+	
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
