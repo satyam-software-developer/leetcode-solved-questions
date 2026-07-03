eee37fa84eecb8524203bd560b7de39686f30bad
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 12:38:40 +0530

    Added solution - LeetHub

---
+++ 706215-find-volume-GFG/706215-find-volume.java
@@ -0,0 +1,19 @@
+class Solution {
+	
+	// Write the first function to
+	int volume(int s) {
+		// return the volume of cube
+		return s * s * s;
+	}
+	
+	// Write the second function to
+	int volume(int l, int w, int h) {
+		// return the volume of the rectangular box
+		return l * w * h;
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
