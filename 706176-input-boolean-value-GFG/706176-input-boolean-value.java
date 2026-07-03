4575731a684a4c6599d7899b3e9465f89f0bef9d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 12:46:59 +0530

    Added solution - LeetHub

---
+++ 706176-input-boolean-value-GFG/706176-input-boolean-value.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean a;
+		
+		// code here
+		a = sc.nextBoolean();
+		
+		// Printing numeric representation
+		System.out.print(a ? 1 : 0);
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
