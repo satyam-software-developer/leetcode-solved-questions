1c103f0bd237b3832605203d20830446d6e33e27
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 12:06:30 +0530

    Added solution - LeetHub

---
+++ 706205-compare-two-numbers-GFG/706205-compare-two-numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// code here
+	int a, b;
+	cin>> a;
+	cin>> b;
+	
+	if (a > b) {
+		cout << 1 << endl;
+	} else {
+		cout << 0 << endl;
+	}
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
