21eaf54ea77b3ebff7ff638ea6005393499675cc
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 13:59:11 +0530

    Added solution - LeetHub

---
+++ 706223-address-operator-GFG/706223-address-operator.cpp
@@ -0,0 +1,15 @@
+void printUsingAddress() {
+	int a;
+	cin>> a;
+	int *addr;
+	
+	// code here
+	addr = &a;
+	
+	cout << *addr ;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
