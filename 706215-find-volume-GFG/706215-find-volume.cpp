85e246b5f38e552a8abcbe247a9960aeec88100d
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 12:39:56 +0530

    Added solution - LeetHub

---
+++ 706215-find-volume-GFG/706215-find-volume.cpp
@@ -0,0 +1,19 @@
+// Write the first function to
+// return the volume of cube
+int volume(int s) {
+	// code here
+	return s * s * s;
+}
+
+// Write the second function to
+// return the volume of the rectangular box
+int volume(int l, int w, int h) {
+	
+	// code here
+	return l * w * h;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
