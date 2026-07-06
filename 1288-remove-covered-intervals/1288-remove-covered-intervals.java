6697362c05233d1501c215724255f4630a07df93
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-06 23:36:56 +0530

    Time: 7 ms (52.31%), Space: 46.2 MB (92.44%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
+            }
+            return b[1] - a[1];
+        });
+
+        int count = 0;
+        int end = 0;
+
+        for (int[] interval : intervals) {
+            if (interval[1] > end) {
+                count++;
+                end = interval[1];
+            }
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
