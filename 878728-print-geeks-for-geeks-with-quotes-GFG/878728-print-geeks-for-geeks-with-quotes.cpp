4bf1de7cfc4e72a4c2c821c4ed048c90adfb0eb3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-07 13:43:18 +0530

    Added solution - LeetHub

---
+++ 878728-print-geeks-for-geeks-with-quotes-GFG/878728-print-geeks-for-geeks-with-quotes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// print "GeeksForGeeks"
+	cout << "\"GeeksForGeeks\"";
+	
+	return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
