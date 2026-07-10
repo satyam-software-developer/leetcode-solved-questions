d046c890f9d0401baafe41a474d282d4d342a9e3
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-10 13:04:47 +0530

    Added solution - LeetHub

---
+++ 706046-sort-in-descending-GFG/706046-sort-in-descending.java
@@ -0,0 +1,21 @@
+class Solution {
+	public void sortInDesc(int[] arr) {
+		// code here
+		Arrays.sort(arr);
+		
+		int i = 0, j = arr.length - 1;
+		while (i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+		
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
