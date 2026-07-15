785a9cf755951fd4bae521db68fb64dd331d1966
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-15 14:52:05 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 878885-calculator-GFG/878885-calculator.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void calculate(int a, int b, int operator) {
+        switch (operator) {
+            case 1:
+                System.out.print(a + b);
+                break;
+            case 2:
+                System.out.print(a - b);
+                break;
+            case 3:
+                System.out.print(a * b);
+                break;
+            default:
+                System.out.print("Invalid Input");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
