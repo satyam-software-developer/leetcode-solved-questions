4ccd137a65d3ebaed313f082e4002f71b046ad7e
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-15 14:55:31 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 878885-calculator-GFG/878885-calculator.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void calculate(int a, int b, int operator_) {
+        switch (operator_) {
+            case 1:
+                cout << a + b;
+                break;
+            case 2:
+                cout << a - b;
+                break;
+            case 3:
+                cout << a * b;
+                break;
+            default:
+                cout << "Invalid Input";
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
