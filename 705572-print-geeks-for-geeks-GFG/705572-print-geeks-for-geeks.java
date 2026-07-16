0d82d9331c8c3fe253ba44d543cd876528688902
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-16 12:39:10 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 705572-print-geeks-for-geeks-GFG/705572-print-geeks-for-geeks.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
