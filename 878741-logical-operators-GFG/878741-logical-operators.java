49fc6f396bbe53705777f52a3b0467751cd6afa0
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-16 13:08:07 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878741-logical-operators-GFG/878741-logical-operators.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String booleanOperations(boolean a, boolean b) {
+        // Code here
+        return (a && b) + " " + (a || b) + " " + (!a);
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
