76670bfe2bbb0006521561fbab21e572450072a9
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-16 12:58:12 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701658-deque-traversal-GFG/701658-deque-traversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void printDeque(deque<int> &dq) {
+        // code here
+        for(int x : dq){
+            cout << x  <<  " ";
+        }
+        cout << endl;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
