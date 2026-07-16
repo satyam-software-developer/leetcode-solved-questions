46e2edf17893a9dc0ae9db8875b1348aac646051
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-16 12:54:49 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701658-deque-traversal-GFG/701658-deque-traversal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for(int x : deq){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
