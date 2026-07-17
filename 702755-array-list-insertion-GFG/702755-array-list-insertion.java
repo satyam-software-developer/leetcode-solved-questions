7cc26d952f36c8d64583873889d38f26dbf7d487
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-17 11:29:23 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 702755-array-list-insertion-GFG/702755-array-list-insertion.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static ArrayList<Integer> fillArrayList(int arr[]) {
+        // Your code here
+        ArrayList<Integer> list  = new ArrayList<>();
+        
+        for(int x : arr){
+            list.add(x);
+        }
+        
+        return list;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
