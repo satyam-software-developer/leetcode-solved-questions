13db3aaebe9fbe5e8538d26e97ec6624fb6bef06
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-17 10:45:31 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878748-day-before-n-days-GFG/878748-day-before-n-days.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int dayBefore(int d, int n) {
+        // code here
+         return (d - (n % 7) + 7) % 7;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
