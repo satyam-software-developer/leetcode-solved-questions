4c34559fc31f88ccc2c3d7c80e5f4e272343ac0c
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-17 10:31:46 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 705323-print-without-newline-GFG/705323-print-without-newline.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void utility(Scanner sc) {
+        // code here
+        String a  = sc.next();
+        String b =  sc.next();
+        
+        System.out.print(a + " " + b);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
