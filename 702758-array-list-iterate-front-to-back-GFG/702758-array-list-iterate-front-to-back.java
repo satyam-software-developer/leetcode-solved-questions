82072f5f796190a1bb6a656a41d229483993f3c9
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-19 14:38:09 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702758-array-list-iterate-front-to-back-GFG/702758-array-list-iterate-front-to-back.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public static void iterateArrayList(Iterator iterator) {
+        // Your code here
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+
+        // Just print the elements, don't provide new line
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
