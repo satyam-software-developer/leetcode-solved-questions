3ff64fd09f3006cbf1d613bc1e7dc0cda03a763b
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-19 23:55:32 +0530

    Added solution - Time: O(n + q), Space: O(n + q)

---
+++ 700549-mountain-subarray-queries-GFG/700549-mountain-subarray-queries.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Boolean> processQueries(int[] arr, int[][] queries) {
+        int n = arr.length;
+
+        int[] up = new int[n];
+        int[] down = new int[n];
+
+        up[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] <= arr[i + 1]) {
+                up[i] = up[i + 1];
+            } else {
+                up[i] = i;
+            }
+        }
+
+        down[n - 1] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= arr[i + 1]) {
+                down[i] = down[i + 1];
+            } else {
+                down[i] = i;
+            }
+        }
+
+        ArrayList<Boolean> ans = new ArrayList<>();
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            ans.add(down[up[l]] >= r);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
