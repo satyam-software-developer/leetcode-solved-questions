df4519b6847966147e2e2cd73d19170d869fd854
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-20 23:48:32 +0530

    Time: 5 ms (83.45%), Space: 47.1 MB (64.37%) - LeetHub - Time: O(m * n), Space: O(m * n)

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+        k %= total;
+
+        int[][] res = new int[m][n];
+
+        for (int i = 0; i < total; i++) {
+            int ni = (i + k) % total;
+            res[ni / n][ni % n] = grid[i / n][i % n];
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(res[i][j]);
+            }
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
