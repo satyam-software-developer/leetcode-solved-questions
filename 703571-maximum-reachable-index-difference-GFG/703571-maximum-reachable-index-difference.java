1a16ca959e5cefac5b48bd6878fadbd05fa6ed3f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-21 23:51:09 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703571-maximum-reachable-index-difference-GFG/703571-maximum-reachable-index-difference.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxIndexDifference(String s) {
+        int n = s.length();
+        int[] maxReach = new int[26];
+        for (int i = 0; i < 26; i++) maxReach[i] = -1;
+
+        int ans = -1;
+        boolean hasA = false;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int c = s.charAt(i) - 'a';
+            int far = i;
+            if (c < 25 && maxReach[c + 1] != -1) {
+                far = maxReach[c + 1];
+            }
+            if (far > maxReach[c]) {
+                maxReach[c] = far;
+            }
+            if (c == 0) {
+                hasA = true;
+                ans = Math.max(ans, far - i);
+            }
+        }
+
+        return hasA ? ans : -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
