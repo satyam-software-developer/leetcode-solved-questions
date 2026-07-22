d0cfa98e267457dba2e8356bd65fb9651b0f6c94
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-22 15:57:00 +0530

    Added solution - Time: O(n + m log m), Space: O(n)

---
+++ 701615-tie-breaker-GFG/701615-tie-breaker.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String tieBreak(String arr[]) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String name : arr) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+
+        String ans = "";
+        int maxVotes = 0;
+
+        for (String name : map.keySet()) {
+            int votes = map.get(name);
+
+            if (votes > maxVotes) {
+                maxVotes = votes;
+                ans = name;
+            } else if (votes == maxVotes && name.compareTo(ans) < 0) {
+                ans = name;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
