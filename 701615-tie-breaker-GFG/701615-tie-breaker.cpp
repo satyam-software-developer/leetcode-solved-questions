3150423e63fbbc036c80e2356f6530196702a949
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-22 15:59:57 +0530

    Added solution - Time: O(N * L), Space: O(N * L)

---
+++ 701615-tie-breaker-GFG/701615-tie-breaker.cpp
@@ -0,0 +1,33 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string tieBreak(vector<string>& arr) {
+        unordered_map<string, int> mp;
+
+        for (string &name : arr) {
+            mp[name]++;
+        }
+
+        string ans = "";
+        int maxVotes = 0;
+
+        for (auto &it : mp) {
+            if (it.second > maxVotes) {
+                maxVotes = it.second;
+                ans = it.first;
+            } 
+            else if (it.second == maxVotes && it.first < ans) {
+                ans = it.first;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
