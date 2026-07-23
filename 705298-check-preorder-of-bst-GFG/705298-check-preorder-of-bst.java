e3b3f4420c94820aa5aef30520581629995cc89b
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-23 23:42:09 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 705298-check-preorder-of-bst-GFG/705298-check-preorder-of-bst.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public boolean canRepresentBST(List<Integer> arr) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MIN_VALUE;
+
+        for (int val : arr) {
+            if (val < root) {
+                return false;
+            }
+
+            while (!stack.isEmpty() && val > stack.peek()) {
+                root = stack.pop();
+            }
+
+            stack.push(val);
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
