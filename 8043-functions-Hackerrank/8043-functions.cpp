b6c5cf36d176c6b194ca95c7088652abaf662bba
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-24 14:43:47 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 8043-functions-Hackerrank/8043-functions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d){
+    
+    int max = a;
+    
+    if(b > max)
+       max = b; 
+    if(c > max)
+       max = c;
+    if(d > max)
+       max = d;
+   
+   return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
